a3ebedacc51c8b3b7bc61578e3a108ba7182f35f
Lataksh Sariya Patidar <latakshpatidar@gmail.com>
2024-09-30 19:16:08 +0530

    Added the solution to the ch1_p1_a.c

---
+++ ch1_p1_a.c
@@ -0,0 +1,16 @@
+// Question : 
+/*
+    Write a C program to calculate area of a rectangle:
+    a. Using hard coded inputs. 
+*/
+
+
+#include <stdio.h>
+
+int main(){
+    int length = 10;
+    int width = 5;
+
+    int area = length * width;
+    printf("The area of a rectange with length: %d and width: %d is %d", length, width, area);
+}
